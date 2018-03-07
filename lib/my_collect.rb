de654c8634a4172b25b0f83a7a5483416128acca
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 20:15:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    yield(array[i])
-    collection << array[i]
+    collection << yield(array[i])
     i += 1
   end
   collection
